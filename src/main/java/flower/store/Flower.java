d9054f1fe967b301efa0040bd57052ae215baa4b
darka0805 <kuzyshyn.pn@ucu.edu.ua>
2023-10-12 21:15:44 +0300

    finished

--- src/main/java/flower/store/Flower.java
+++ src/main/java/flower/store/Flower.java
@@ -1,10 +1,30 @@
 package flower.store;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+// @Getter @AllArgsConstructor @ToString @NoArgsConstructor
+// public class Flower{
+//     private FlowerType flowerType;
+//     private double price;
+//     private double sepalLength;
+//     private FlowerColor color;
 
-
+//     public Flower(Flower flower){
+//         this.flowerType = flower.flowerType;
+//         this.price = flower.price;
+//         this.sepalLength = flower.sepalLength;
+//         this.color = flower.color;
+//     }
+//     // public String getColor() {
+//     //     return color.toString();
+//     // }
+// }
+@NoArgsConstructor
+@AllArgsConstructor
 @Setter
 public class Flower {
     @Getter
@@ -14,6 +34,13 @@ public class Flower {
     private double price;
     @Getter
     private FlowerType flowerType;
+
+    public Flower(Flower flower){
+        this.flowerType = flower.flowerType;
+        this.price = flower.price;
+        this.sepalLength = flower.sepalLength;
+        this.color = flower.color;
+    }
 
     public String getColor() {
         return color.toString();
