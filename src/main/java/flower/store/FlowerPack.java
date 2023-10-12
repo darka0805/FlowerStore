ec4f512cd933932b51238324ee3ec6922d0a46a1
darka0805 <kuzyshyn.pn@ucu.edu.ua>
2023-10-12 21:39:02 +0300

    new_new_new_new

--- src/main/java/flower/store/FlowerPack.java
+++ src/main/java/flower/store/FlowerPack.java
@@ -11,11 +11,11 @@ public class FlowerPack {
         this.flower = new Flower(flower);
         this.quantity = quantity;
     }
-    public void setQuantity(int quantity2) {
-        if (quantity2 < 1) {
+    public void setQuantity(int quantity_2) {
+        if (quantity_2 < 1) {
             this.quantity = 1;
         } else {
-            this.quantity = quantity2;
+            this.quantity = quantity_2;
         }
     }
 
